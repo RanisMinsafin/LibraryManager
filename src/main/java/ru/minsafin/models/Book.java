608e77a23433bad39987e314bf8565eab083528d
Ranis Minsafin <bobbyzz999@gmail.com>
2024-01-21 01:18:14 +0300

    book model & pom changes

--- src/main/java/ru/minsafin/models/Book.java
+++ src/main/java/ru/minsafin/models/Book.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.ToString;
 
-import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
